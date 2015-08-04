0b741d7d42f710514bc4dba33d91c8c459596bb8
avifoxi <frosenox@gmail.com>
2015-08-04 15:07:50 -0400

    watchify, and move scss into partials in sass; ;

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
- 
-// Basic usage 
-gulp.task('scripts', function() {
-  // Single entry point to browserify 
-  gulp.src('source/javascript/REFACTOR.js')
-    .pipe(browserify({
-      insertGlobals : true,
-      debug : !gulp.env.production
+var watchify = require('gulp-watchify')
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash').assign;
+
+
+/* 
+
+  JS WATCHIFY / BROWSERIFY FOR DEV BUILD
+
+*/
+
+var watching = false
+gulp.task('enable-watch-mode', function() { watching = true })
+
+gulp.task('browserify', watchify(function(watchify) {
+  return gulp.src('./source/app/REFACTOR.js')
+    .pipe(watchify({
+        watch:watching
     }))
-    .pipe(gulp.dest('./dist/javascript'))
-});
+    .pipe(gulp.dest('./dist/app/'))
+}))
+
+gulp.task('watchify', ['enable-watch-mode', 'browserify'])
+
+gulp.task('watch', ['watchify'], function () {
+    // ... other watch code ...
+})
+// gulp.task('watchScripts', function() {
+//   gulp.watch('src/**/*.js', ['scripts']);
+// });
--- source/app/REFACTOR.js
+++ source/app/REFACTOR.js
@@ -1,3 +1,5 @@
 window.$ = require('jquery');
 
 var ERS = {};
+
+// console.log('foo this bar yo');
